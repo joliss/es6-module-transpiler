3f3a25191ccb8da89c398e499e263b2ae34d44e9
Jo Liss <joliss42@gmail.com>
2013-12-02 13:51:25 +0000

    Add semicolons_optional.amd.js test

---
+++ test/features/semicolons_optional.amd.js
@@ -0,0 +1,6 @@
+define(
+  ["jquery","rsvp","exports"],
+  function(__dependency1__, __dependency2__, __exports__) {
+    "use strict";
+    var ajax = __dependency1__.ajax;__exports__.defer = __dependency2__.defer;
+  });
